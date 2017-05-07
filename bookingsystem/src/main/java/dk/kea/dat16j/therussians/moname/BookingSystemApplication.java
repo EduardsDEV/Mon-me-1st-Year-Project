64b7677d9923fcc751addc513212b831aecdb357
Epharas <cbetivu@gmail.com>
2017-05-07 14:45:31 +0200

    Initilalize Spring Project

---
+++ bookingsystem/src/main/java/dk/kea/dat16j/therussians/moname/BookingSystemApplication.java
@@ -0,0 +1,12 @@
+package dk.kea.dat16j.therussians.moname;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookingSystemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BookingSystemApplication.class, args);
+	}
+}
